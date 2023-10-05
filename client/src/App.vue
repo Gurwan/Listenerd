d7cd83af8f2114d221023661a4b26edd31b3ad25
Gurwan <gurwandelaunay35@gmail.com>
2023-10-05 16:55:49 +0100

    create simple view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <main-navbar></main-navbar>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainNavbar from './components/MainNavbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    "main-navbar": MainNavbar
   }
 }
 </script>
---
+++ client/src/components/MainNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <a>href="#">Home</a>
+    <div>
+      <ul>
+        <li>
+          <a>>href="#">Search</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+        <li>
+          <a href="#">Register</a>
+        </li>      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MainNavbar'
+};
+</script>
