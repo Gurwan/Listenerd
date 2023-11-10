9e56fd7c5870fff7a56d426935dd9a11f3752c9b
Gurwan <gurwandelaunay35@gmail.com>
2023-11-10 11:25:46 +0000

    add security check on register feature

--- client/src/components/RegisterPage.vue
+++ client/src/components/RegisterPage.vue
@@ -5,11 +5,23 @@
       <div class="bg-white p-8 rounded">
         <h2 class="text-2xl font-semibold mb-4">Register</h2>
         <form @submit.prevent="register">
+          <div>
+            <span style="color: red;" v-if="username.length < 4">The username must contain at least 4 characters</span>
+          </div>
           <div class="mb-4">
             <input type="text" id="username" placeholder="Username" v-model="username" class="form-input">
           </div>
+          <div>
+            <span style="color: red;" v-if="password.length < 8">The password must contain at least 8 characters</span>
+          </div>
           <div class="mb-4">
             <input type="password" id="password" placeholder="Password" v-model="password" class="form-input">
+          </div>
+          <div>
+            <span style="color: red;" v-if="password !== repeat_password">Both passwords must be identical</span>
+          </div>
+          <div class="mb-4">
+            <input type="password" id="repeat_password" placeholder="Confirm your password" v-model="repeat_password" class="form-input">
           </div>
           <div class="mt-6">
             <button type="submit" class="big-button bg-blue-500 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue focus:border-blue-300">
@@ -35,16 +47,28 @@ export default {
     return {
       username: '',
       password: '',
+      repeat_password: ''
     };
   },
   methods: {
     async register() {
-      await axios.post('http://localhost:3001/user', {
-        username: this.username,
-        password: this.password,
-      });
-
-      router.push('/login');
+      if(this.username.length >= 4){
+        if(this.password == this.repeat_password){
+          if(this.password.length >= 8){
+            await axios.post('http://localhost:3001/user', {
+              username: this.username,
+              password: this.password,
+            });
+            router.push('/login');
+          } else {
+            alert('The password must contain at least 8 characters');
+          }
+        } else {
+          alert('Both passwords must be identical');
+        }
+      } else {
+          alert('The username must contain at least 4 characters');
+      }
     },
   },
 };
