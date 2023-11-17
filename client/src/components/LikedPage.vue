b432cc7b40d1733af536db0109874b9fd173dab9
Gurwan <gurwandelaunay35@gmail.com>
2023-11-17 11:15:11 +0000

    filter on liked list

--- client/src/components/LikedPage.vue
+++ client/src/components/LikedPage.vue
@@ -3,14 +3,12 @@
       <main-navbar></main-navbar>
       <div class="filter-bar">
         <div class="filterby-div">
-          <p>FILTER BY</p>
-          <select class="bg-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option selected value="">YEAR</option>
-            <option></option>
+          <p>FILTER</p>
+          <select @change="changeYearFilter" id="selectYear" class="bg-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option selected value="defaultValue">BY YEAR</option>
           </select>
-          <select class="bg-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option selected value="">ARTIST</option>
-            <option></option>
+          <select @change="changeArtistFilter" id="selectArtist" class="bg-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option selected value="defaultValue">BY ARTIST</option>
           </select>
         </div>
         <div class="sortby-div">
@@ -25,7 +23,7 @@
         </div>
       </div>
       <div class="grid-list">
-        <div v-for="data in sortAlbumsList" :key="data[0]" class="p-4 flex flex-col items-center">
+        <div v-for="data in sortAndFilterAlbumsList" :key="data[0]" class="p-4 flex flex-col items-center">
           <router-link :to="'/album/' + data[0]" >
             <img :src="data[3]" alt="Image" class="max-w-full h-auto w-64 md:w-48 lg:w-32 xl:w-24 mb-2 mx-auto"> 
             <p class="font-bold">{{ data[1] }}</p>
@@ -45,7 +43,7 @@
           </div>
         </div>
       </div>
-      <div v-if="sortAlbumsList.length">
+      <div v-if="sortAndFilterAlbumsList.length">
         <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" @click="clearLikedList">
           <i style="padding: 1rem;" class="fa-solid fa-trash"></i> <span style="padding:1rem">Clear your liked albums list</span></button>
       </div>
@@ -65,6 +63,8 @@ data() {
     params: null,
     sort: {by: null, order:'asc'},
     listButton: false,
+    filterYear: null,
+    filterArtist: null
   };
 },
 created(){
@@ -77,6 +77,33 @@ created(){
       const allData = response.data.resultLiked;
       if (Array.isArray(allData) && allData.length > 0) {
         this.albumsValues = allData;
+        let yearList = []
+        let artistList = []
+        for(let a in allData){
+          let year = allData[a][4].split("-")[0]
+          yearList.push(year);
+          artistList.push(allData[a][2][1])
+        }
+        const yearListUnique = new Set(yearList);
+        const artistListUnique = new Set(artistList);
+        const selectYear = document.getElementById("selectYear");
+        const selectArtist = document.getElementById("selectArtist");
+        let artistSortedArray = Array.from(artistListUnique).sort();
+        let yearSortedArray = Array.from(yearListUnique).sort((a, b) => b - a);
+
+        for (const y of yearSortedArray) {
+          let option = document.createElement("option");
+          option.value = y;
+          option.text = y;
+          selectYear.appendChild(option);
+        }
+
+        for (const a of artistSortedArray) {
+          let option = document.createElement("option");
+          option.value = a;
+          option.text = a;
+          selectArtist.appendChild(option);
+        }
       } else {
         console.error('API data error');
       }
@@ -89,8 +116,13 @@ created(){
     });
   },
   computed: {
-    sortAlbumsList(){
-      return this.sortList();
+    sortAndFilterAlbumsList(){
+      const retSort = this.sortList();
+      if(this.filterArtist == null && this.filterYear == null){
+        return retSort;
+      } else {
+        return this.filterList(retSort);
+      }
     }
   },
   methods: {
@@ -144,6 +176,51 @@ created(){
           }
         })
       }
+    },
+    changeYearFilter(e){
+      const v = e.target.value;
+      if(v == "defaultValue"){
+        this.filterYear = null;
+      } else {
+        this.filterYear = v;
+      }
+    },
+    changeArtistFilter(e){
+      const v = e.target.value;
+      if(v == "defaultValue"){
+        this.filterArtist = null;
+      } else {
+        this.filterArtist = v;
+      }
+    },
+    filterList(list){
+      let retList = []
+      for(let a in list){
+        if(this.filterYear != null){
+          if(list[a][4].split("-")[0] == this.filterYear){
+            if(this.filterArtist == null){
+              retList.push(list[a]);
+            } else {
+              if(list[a][2][1] == this.filterArtist){
+                retList.push(list[a]);
+              }
+            }
+          }
+        } else if(this.filterArtist != null){
+          if(list[a][2][1] == this.filterArtist){
+            if(this.filterYear == null){
+              retList.push(list[a]);
+            } else {
+              if(list[a][4].split("-")[0] == this.filterArtist){
+                retList.push(list[a]);
+              }
+            }
+          }
+        } else {
+          retList.push(list[a]);
+        }
+      }
+      return retList;
     },
     hideShow(){
       if(this.listButton){
